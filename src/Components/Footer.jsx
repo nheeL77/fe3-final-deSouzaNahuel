182ec126394a619a701188fc4e9ff9e864763dd4
nheeL77 <nahuel.ivaan@gmail.com>
2023-09-15 00:14:46 -0300

    arreglo css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import logo from '../images/DH.png'
 import { useContextGlobal } from './utils/global.context'
+
+import logo from '../images/DH.png'
+import facebook from '../images/ico-facebook.png'
+import instagram from '../images/ico-instagram.png'
+import tiktok from '../images/ico-tiktok.png'
+import whatsapp from '../images/ico-whatsapp.png'
 
 const Footer = () => {
   const {themeState} = useContextGlobal();
+
   return (
+    
     <footer className={themeState.theme} style={{background: 'red'}}>
-        <p>Powered by</p>
-        <img src={logo} alt='DH-logo' />
+      <p>Powered by</p>
+      <img src={logo} alt='DH-logo' />
+      <img src={facebook} alt='ico-facebook'/>
+      <img src={instagram} alt='ico-instagram'/>
+      <img src={tiktok} alt='ico-tiktok'/>
+      <img src={whatsapp} alt='ico-whatsapp'/>
     </footer>
   )
 }
